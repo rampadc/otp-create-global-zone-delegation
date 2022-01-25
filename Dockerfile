1e4edaa9b8f4cfb9945d15708c6828950ecf360d
Cong Nguyen <rampadc@users.noreply.github.com>
2022-01-25 22:27:50 +1100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM registry.access.redhat.com/ubi8/ubi:8.5-214
+
+WORKDIR ~
+
+RUN yum install -y unzip
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+RUN unzip awscliv2.zip
+RUN ./aws/install
+
+RUN curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz -o "oc.tar.gz"
+RUN tar xzf oc.tar.gz
+RUN mv oc /usr/local/bin/oc
+
+RUN curl "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux32" -o "jq"
+RUN mv jq /usr/local/bin/jq
+
+RUN rm oc.tar.gz awscliv2.zip
+
+COPY delegation-record.json delegation-record.json
+COPY main.sh main.sh
+RUN chmod +x main.sh
+
+CMD ["./main.sh"]
---
+++ main.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "---- Logging into provided managed cluster $managedClusterName ----"
+clusterSecret=$(oc get clusterdeployment $managedClusterName -n $managedClusterName -o jsonpath='{.spec.clusterMetadata.adminPasswordSecretRef.name}')
+username=$(oc get secret $clusterSecret  -n $managedClusterName -o jsonpath="{.data.username}" | base64 --decode)
+password=$(oc get secret $clusterSecret  -n $managedClusterName -o jsonpath="{.data.password}" | base64 --decode)
+apiURL=$(oc get clusterdeployment -n $managedClusterName -o jsonpath="{.items[0].status.apiURL}")
+
+oc login $apiURL -u $username -p $password --insecure-skip-tls-verify
+
+echo "---- Getting AWS ID and Key from managed cluster $managedClusterName ----"
+export AWS_ACCESS_KEY_ID=$(oc get secret -n $managedClusterName $managedClusterName-aws-creds -o=jsonpath='{.data.aws_access_key_id}' | base64 --decode)
+export AWS_SECRET_ACCESS_KEY=$(oc get secret -n $managedClusterName $managedClusterName-aws-creds -o=jsonpath='{.data.aws_secret_access_key}' | base64 --decode)
+echo "Using access key $AWS_ACCESS_KEY_ID"
+aws configure
+
+echo "---- Configuring route53 based on $managedClusterName's base domain ----"
+
+# Run on an AWS managed cluster
+export cluster_base_domain=$(oc get dns cluster -o jsonpath='{.spec.baseDomain}')
+echo "cluster_base_domain: $cluster_base_domain"
+
+export cluster_zone_id=$(oc get dns cluster -o jsonpath='{.spec.publicZone.id}')
+echo "cluster_zone_id: $cluster_zone_id"
+
+export global_base_domain=global.${cluster_base_domain#*.}
+echo "global_base_domain: $global_base_domain"
+
+aws route53 create-hosted-zone --name ${global_base_domain} --caller-reference $(date +"%m-%d-%y-%H-%M-%S-%N")
+
+export global_zone_res=$(aws route53 list-hosted-zones-by-name --dns-name ${global_base_domain} | jq -r .HostedZones[0].Id )
+echo "global_zone_res: $global_zone_res"
+
+export global_zone_id=${global_zone_res##*/}
+echo "global_zone_id: $global_zone_id"
+
+export delegation_record=$(aws route53 list-resource-record-sets --hosted-zone-id ${global_zone_id} | jq .ResourceRecordSets[0])
+echo "delegation_record: $delegation_record"
+
+envsubst < ./delegation-record.json > ./delegation-record.json
+cat ./delegation-record.json
+
+aws route53 change-resource-record-sets --hosted-zone-id ${cluster_zone_id} --change-batch ./delegation-record.json
